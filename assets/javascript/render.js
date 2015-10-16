24de43d66bcfda6c4568af040cc86608782e4ea7
Ahad Wasim <ahadwasim@gmail.com>
2015-10-16 16:10:38 -0700

    Displaying Tweets on Main Section

--- assets/javascript/render.js
+++ assets/javascript/render.js
@@ -10,7 +10,6 @@ $(function(){
     var $body = $('body');
 
     console.log(streams);
-    console.log(streams.home.length);
 
     // TRAVERSAL IS FASTER THAN SELECTORS
     var userContainer = $('#user-container').children('li');
@@ -35,16 +34,21 @@ $(function(){
 
 
 
-
-
     function updateTweetCount(){
       var userCount = 0;
+      
+
       _.each(streams.users,function(tweetsArray,name,list){
+        var grabHashCount = $(hashCount[userCount]);
+
         if(tweetsArray.length > 1000){
-          $(hashCount[userCount]).text('1000+'); 
-        } else {
-          $(hashCount[userCount]).text(tweetsArray.length);
+          grabHashCount.text('1000+'); 
+        } else if( parseInt(grabHashCount.text())  !== tweetsArray.length){
+          grabHashCount.fadeOut('slow',function(){
+            $(this).text(tweetsArray.length).fadeIn('slow');
+          });
         }
+        
         userCount++;
       });
     }
@@ -56,32 +60,66 @@ $(function(){
 
 
 
-
-
-
     $('#user-container').on('click','li',function(){
       // Display the modal for the users timeline
+      // function displayModal 
     });
+
+    $('.Main-Content-Tweet-List').on('click','.USERNAME',function(){
+        
+        var name = $(this).text().slice(1,this.length);
+
+        // Call the displayModal function like the one above.
+    })
 
 
     $('#user-container').on('mouseenter','li',toggleName);
 
-    $('#user-container').on('mouseleave','li',toggleName)
+    $('#user-container').on('mouseleave','li',toggleName);
+
 
     
 
 
     // When the browser first loads our HTML it starts off with only 11 tweets. But new tweets are getting generated real time because of the data_generator.js file.
     // Thats why a while loop is getting created because we have a permanent number at one point.
-    // Whereas if we used a foor loop. It we would be checking the length of the array all the time. And the array will always be changing. With the while loop were just sticking with one of the lengths.
+    // Whereas if we used a for loop. It we would be checking the length of the array all the time. And the array will always be changing. With the while loop were just sticking with one of the lengths.
+    // It starts off from the back of the array because the last element in the array is the latest message
+
+
+
 
     var index = streams.home.length - 1;
-    while(index >= 0){
-      var tweet = streams.home[index];
-      var $tweet = $('<div></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message);
-      $tweet.appendTo($body);
-      index -= 1;
+
+    while(index >=0){
+
+      var user =  streams.home[index].user;
+      var message = streams.home[index].message;
+      var timeCreated = streams.home[index].created_at;
+      // put this code in a function and call a setInterval for every minute
+      var displayTime = moment(timeCreated).startOf().fromNow();
+
+
+      // Creating the tweet elements for the home-page.
+
+      var userContentHeader = $('<div></div>').addClass('User-Content-Header');
+      var tempImg = $('<img>').attr({'src':'http://whatsupintheworld.com/wp-content/uploads/2014/06/Facebook-Blank-Photo.jpg'});
+      var homeTweetUser = $('<h3>' + user + '</h3>');
+      var homeTweetUserLink = $('<span>@' + user  +'</span>');
+      var timePosted = $('<small></small>').text(displayTime);
+
+      userContentHeader.append(tempImg,[homeTweetUser,homeTweetUserLink,timePosted]);
+
+      var messegeElement = $('<p></p>').text(message);
+
+      var userContentWrapper = $('<div></div>').attr({'class':'User-Content','data-user':user});
+      userContentWrapper.append(userContentHeader,message);
+
+      var eachTweetWrapper = $('<li></li>').append(userContentWrapper)
+
+
+      $('.Main-Content-Tweet-List').append(eachTweetWrapper);
+      index--;
     }
 
 });
