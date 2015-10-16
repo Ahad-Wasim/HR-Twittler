bbee8363106b9f629f09db9fe0b40605084ca53a
Ahad Wasim <ahadwasim@gmail.com>
2015-10-15 21:26:56 -0700

    Finished the User Bar

--- assets/javascript/render.js
+++ assets/javascript/render.js
@@ -1,8 +1,74 @@
+// Load this code even before the DOM, it won't affect the DOM elemenents.
+function toggleName(event){
+  $(this).find('.hover-name').slideToggle(200);  
+}
+
+
+
 $(function(){
+
     var $body = $('body');
 
     console.log(streams);
     console.log(streams.home.length);
+
+    // TRAVERSAL IS FASTER THAN SELECTORS
+    var userContainer = $('#user-container').children('li');
+
+    var userNameTags = userContainer.find('h4');
+    var hoverTags = userContainer.find('.hover-name')
+    var hashCount = userContainer.find('.user-info-wrapper').find('.tweets-number');
+
+
+    (function(){
+      var userCount = 0;
+
+      _.each(streams.users,function(eachTweet,name,list){
+
+        $(userNameTags[userCount]).text(name);
+        $(hoverTags[userCount]).text(name);
+        $(hashCount[userCount]).text(eachTweet.length);
+         
+        userCount++;
+      });
+    }());
+
+
+
+
+
+    function updateTweetCount(){
+      var userCount = 0;
+      _.each(streams.users,function(tweetsArray,name,list){
+        if(tweetsArray.length > 1000){
+          $(hashCount[userCount]).text('1000+'); 
+        } else {
+          $(hashCount[userCount]).text(tweetsArray.length);
+        }
+        userCount++;
+      });
+    }
+
+    // Every three seconds it will update the number of tweets
+    setInterval(updateTweetCount,3000);
+
+
+
+
+
+
+
+
+    $('#user-container').on('click','li',function(){
+      // Display the modal for the users timeline
+    });
+
+
+    $('#user-container').on('mouseenter','li',toggleName);
+
+    $('#user-container').on('mouseleave','li',toggleName)
+
+    
 
 
     // When the browser first loads our HTML it starts off with only 11 tweets. But new tweets are getting generated real time because of the data_generator.js file.
